0dd206abadc6dee9c6579a265212b78a4b1db86c
James Andrew McEniry <jamesandrewmceniry@Jamess-MacBook-Pro.local>
2015-11-11 13:27:22 -0800

    Add style and images to your game assignment

--- game.js
+++ game.js
@@ -1,118 +1,187 @@
 
+var questions = [
+
+	"Yes or No: did I grow up in Seattle?",
+	"Yes or No: did I attend UW",
+	"Yes or No: is baseball my favorite sport?",
+	"Yes or No: is football my favorite sport?",
+	"What is 20 + 3?"
+
+];
+
+var answers = ["yes", "y", "no", "n", "23"];
+
+var numberCorrect = 0;
+
+
 function askQuestionOne() {
 
-	var questionOne = prompt("Yes or No: did I grow up in Seattle?");
+	var questionOne = prompt(questions[0]);
 	questionOne = questionOne.toLowerCase();
 
-	var numberCorrect = 0;
+	var answerOne = document.getElementById('questionOne');
 
-	if (questionOne === "yes" || questionOne === "y") {
+	if (questionOne === answers[0] || questionOne === answers[1]) {
 
 		numberCorrect ++;
-		alert("Correct!");
+		// alert("Correct!");
+
+		answerOne.innerHTML += ' Correct!';
+		answerOne.style.color = 'green';
+
+		document.getElementById("image1").src = "http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png";
 
 	}
 
 	else {
 
-		alert("Incorrect!");
+		// alert("Incorrect!");
+
+		answerOne.innerHTML += ' Incorrect!';
+		answerOne.style.color = 'red';
+
+		document.getElementById("image1").src = "http://www.iconsdb.com/icons/preview/soylent-red/x-mark-xxl.png";
 
 	}
 
-    document.getElementById('questionOne').innerHTML += ' Yes';
-	askQuestionTwo();
+	
+
 }
 
 function askQuestionTwo() {	
 
-	var questionTwo = prompt("Yes or No: did I attend UW");
+	var questionTwo = prompt(questions[1]);
 	questionTwo = questionTwo.toLowerCase();
 
-	if (questionTwo === "yes" || questionTwo === "y") {
+	var answerTwo = document.getElementById('questionTwo');
+
+	if (questionTwo === answers[0] || questionTwo === answers[1]) {
 
 		numberCorrect ++;
-		alert("Correct!");
+		// alert("Correct!");
+
+		answerTwo.innerHTML += ' Correct!';
+		answerTwo.style.color = 'green';
+
+		document.getElementById("image2").src = "http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png";
+
 
 	}
 
 	else {
 
-		alert("Incorrect!");
+		// alert("Incorrect!");
+
+		answerTwo.innerHTML += ' Incorrect!';
+		answerTwo.style.color = 'red';
+
+		document.getElementById("image2").src = "http://www.iconsdb.com/icons/preview/soylent-red/x-mark-xxl.png";
 	}
 
-	document.getElementById('questionTwo').innerHTML += ' Yes';
-	askQuestionThree();
 }
 
 function askQuestionThree() {
 
-	var questionThree = prompt("Yes or No: is baseball my favorite sport?");
+	var questionThree = prompt(questions[2]);
 	questionThree = questionThree.toLowerCase();
 
-	if (questionThree === "no" || questionThree === "n") {
+	var answerThree = document.getElementById('questionThree');
+
+	if (questionThree === answers[2] || questionThree === answers[3]) {
 
 		numberCorrect ++;
-		alert("Correct!");
+		// alert("Correct!");
+
+		answerThree.innerHTML += ' Correct!';
+		answerThree.style.color = 'green';
+
+		document.getElementById("image3").src = "http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png";
 
 	}
 
 	else {
 
-		alert("Incorrect!");
+		// alert("Incorrect!");
+
+		answerThree.innerHTML += ' Incorrect!';
+		answerThree.style.color = 'red';
+
+		document.getElementById("image3").src = "http://www.iconsdb.com/icons/preview/soylent-red/x-mark-xxl.png";
 
 	}
 
-	document.getElementById('questionThree').innerHTML += ' No';
-	askQuestionFour();
 }
 
 function askQuestionFour() {
 
-	var questionFour = prompt("Yes or No: is football my favorite sport?");
+	var questionFour = prompt(questions[3]);
 	questionFour = questionFour.toLowerCase();
 
-	if (questionFour === "yes" || questionFour === "y") {
+	var answerFour = document.getElementById('questionFour');
+
+	if (questionFour === answers[0] || questionFour === answers[1]) {
 
 		numberCorrect ++;
-		alert("Correct!");
+		// alert("Correct!");
+
+		answerFour.innerHTML += ' Correct!';
+		answerFour.style.color = 'green';
+
+		document.getElementById("image4").src = "http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png";
 
 	}
 
 	else {
 
-		alert("Incorrect!");
+		// alert("Incorrect!");
+
+		answerFour.innerHTML += ' Incorrect!';
+		answerFour.style.color = 'red';
+
+		document.getElementById("image4").src = "http://www.iconsdb.com/icons/preview/soylent-red/x-mark-xxl.png";
 
 	}
 
-	document.getElementById('questionFour').innerHTML += ' Yes';
-	askQuestionFive();
 }
 
 function askQuestionFive() {
 
-	var questionFive = prompt("What is 20 + 3?");
+	var questionFive = prompt(questions[4]);
 
-	if (questionFive === "23") {
+	var answerFive = document.getElementById('questionFive');
+
+	if (questionFive === answers[4]) {
 
 		numberCorrect ++;
-		alert("Correct!");
+		// alert("Correct!");
+
+		answerFive.innerHTML += ' Correct!';
+		answerFive.style.color = 'green';
+
+		document.getElementById("image5").src = "http://www.iconsdb.com/icons/preview/guacamole-green/checkmark-xxl.png";
 
 	}
 
 	else {
 
-		alert("Incorrect!");
+		// alert("Incorrect!");
+
+		answerFive.innerHTML += ' Incorrect!';
+		answerFive.style.color = 'red';
+
+		document.getElementById("image5").src = "http://www.iconsdb.com/icons/preview/soylent-red/x-mark-xxl.png";
 
 	}
 
-	document.getElementById('questionFive').innerHTML += ' 23';
-
-	alert("You got " + numberCorrect + " correct!");
+	document.getElementById('numberCorrect').innerHTML = "You got " + numberCorrect + " correct!";
 
 }
 
 askQuestionOne();
-
+askQuestionTwo();
+askQuestionThree();
+askQuestionFour();
+askQuestionFive();
 
 
 
